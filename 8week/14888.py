07394f7223c0fe15c02c5fb4105c672385519c22
bami-jibsa <tlswjddn20060615@gmail.com>
2024-04-21 20:04:50 +0900

    commit

--- 8week/14888.py
+++ 8week/14888.py
@@ -1,17 +1,32 @@
+import sys
 N = int(input())
 
 num = list(map(int, input().split()))
 
-
 ops = list(map(int, input().split()))
 
-def btr()
+maximum = -1e9
+minimum = 1e9
 
-#크게 
-# 최대한 큰수끼리 곱하고 작은수로 나누기
-#작게 
-# 최대한 큰수끼리 나누고 작은수로 곱하기.
+def btr(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+    if plus:
+        btr(depth + 1,total + num[depth], plus - 1, minus, multiply, divide)
+    
+    if plus:
+        btr(depth + 1,total - num[depth], plus, minus - 1, multiply, divide)
+    if plus:
+        btr(depth + 1,total + num[depth], plus, minus, multiply - 1, divide)
+    if plus:
+        btr(depth + 1,int(total) / num[depth], plus, minus, multiply, divide - 1)
 
+btr(1, num[0], ops[0], ops[1], ops[2], ops[3])
+print(maximum)
+print(minimum)
 
 
 
