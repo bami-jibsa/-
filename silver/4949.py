5c58d7c201260ab335c0f88d69560ecd5547df9f
bami-jibsa <tlswjddn20060615@gmail.com>
2024-05-28 15:29:17 +0900

    8week

---
+++ silver/4949.py
@@ -0,0 +1,20 @@
+def cal(a):
+    big = []
+    for i in a:
+        if i in '([':
+            big.append(i)
+        elif i == ')':
+            if not big or big[-1] != '(':
+                return False
+            big.pop()
+        elif i == ']':
+            if not big or big[-1] != '[':
+                return False
+            big.pop()
+    return not big
+
+while True:
+    li = input().rstrip()
+    if li == '.':
+        break
+    print("yes" if cal(li) else 'no')
