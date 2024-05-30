73b515916110f0fda54ba7b38ad6aed663ae1b85
bami-jibsa <tlswjddn20060615@gmail.com>
2024-05-30 11:01:07 +0900

    8week

---
+++ bronze/2292.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n == 1:
+    print(1)
+else:
+    layer = 1  
+    cnt = 1 
+    
+    while cnt < n:
+        cnt += 6 * layer  
+        layer += 1
+    
+    print(layer)
---
+++ bronze/2903.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+a = 2 ** n + 1
+points = a ** 2
+
+print(points)
